9fe546558a4aa6b2c604f4c483ce8a24e900d4d9
huihuihui07 <huifeng001@gmail.com>
2023-01-31 13:40:27 +0000

    update

--- script.js
+++ script.js
@@ -1,28 +1,23 @@
-class Timer {
-  constructor(durationInput, startButton, pauseButton) {
-    this.durationInput = durationInput;
-    this.startButton = startButton;
-    this.pauseButton = pauseButton;
-
-    this.startButton.addEventListener("click", this.start);
-    this.pauseButton.addEventListener("click", this.pause);
-  }
-
-  start = () => {
-    this.tick;
-    this.interval = setInterval(this.tick, 1000);
-  };
-
-  pause = () => {
-    clearInterval(this.interval);
-  };
-
-  tick = () => {
-    console.log("tick");
-  };
-}
-
 const durationInput = document.querySelector("#duration");
 const startButton = document.querySelector("#start");
 const pauseButton = document.querySelector("#pause");
-const timer = new Timer(durationInput, startButton, pauseButton);
+const circle = document.querySelector("circle");
+
+const perimeter = circle.getAttribute("r") * 2 * Math.PI;
+circle.setAttribute("stroke-dasharray", perimeter);
+
+const timer = new Timer(durationInput, startButton, pauseButton, {
+  //the callback object tells the outside world that the Start/Tick/Complete events occurs
+
+  onStart() {
+    console.log("timer started");
+  },
+
+  onTick() {
+    console.log("ticking down");
+  },
+
+  onComplete() {
+    console.log("timer completed");
+  },
+});
