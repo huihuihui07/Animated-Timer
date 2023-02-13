39bc88bc56a22b80a850fed9633ba8129d97fe25
huihuihui07 <huifeng001@gmail.com>
2023-02-13 10:48:53 +0000

    update

--- Timer.js
+++ Timer.js
@@ -1,54 +1,56 @@
 class Timer {
-    constructor(durationInput, startButton, pauseButton, callbacks) {
-      this.durationInput = durationInput;
-      this.startButton = startButton;
-      this.pauseButton = pauseButton;
-  
-      //link the callbacks from the callback object to the Timer Object
-      if (callbacks) {
-        this.onStart = callbacks.onStart;
-        this.onTick = callbacks.onTick;
-        this.onComplete = callbacks.onComplete;
-      }
-  
-      //add eventListener to the methods
-      this.startButton.addEventListener("click", this.start);
-      this.pauseButton.addEventListener("click", this.pause);
+  constructor(durationInput, startButton, pauseButton, callbacks) {
+    this.durationInput = durationInput;
+    this.startButton = startButton;
+    this.pauseButton = pauseButton;
+
+    //link the callbacks from the callback object to the Timer Object
+    if (callbacks) {
+      this.onStart = callbacks.onStart;
+      this.onTick = callbacks.onTick;
+      this.onComplete = callbacks.onComplete;
     }
-  
-    start = () => {
-      if (this.onStart) {
-        this.onStart();
-      }
-      this.tick;
-      this.interval = setInterval(this.tick, 1000);
-    };
-  
-    pause = () => {
-      clearInterval(this.interval);
-    };
-  
-    tick = () => {
-      if (this.timeRemaining <= 0) {
-        this.pause();
-  
-        if (this.onComplete) {
-          this.onComplete();
-        }
-      } else {
-        this.timeRemaining = this.timeRemaining - 1;
-  
-        if (this.onTick) {
-          this.onTick();
-        }
-      }
-    };
-  
-    get timeRemaining() {
-      return parseFloat(this.durationInput.value);
-    }
-  
-    set timeRemaining(time) {
-      this.durationInput.value = time;
-    }
+
+    //add eventListener to the methods
+    this.startButton.addEventListener("click", this.start);
+    this.pauseButton.addEventListener("click", this.pause);
   }
+
+  start = () => {
+    if (this.onStart) {
+      this.onStart();
+    }
+    this.tick;
+    //tick every 10 mili-second
+    this.interval = setInterval(this.tick, 10);
+  };
+
+  pause = () => {
+    clearInterval(this.interval);
+  };
+
+  tick = () => {
+    if (this.timeRemaining <= 0) {
+      this.pause();
+
+      if (this.onComplete) {
+        this.onComplete();
+      }
+    } else {
+      //tick every 10 mili-second. round the float number to 2 digits.
+      this.timeRemaining = Math.round((this.timeRemaining - 0.01) * 100) / 100;
+
+      if (this.onTick) {
+        this.onTick();
+      }
+    }
+  };
+
+  get timeRemaining() {
+    return parseFloat(this.durationInput.value);
+  }
+
+  set timeRemaining(time) {
+    this.durationInput.value = time;
+  }
+}
--- script.js
+++ script.js
@@ -1,10 +1,21 @@
+//connect to the DOM elements
 const durationInput = document.querySelector("#duration");
 const startButton = document.querySelector("#start");
 const pauseButton = document.querySelector("#pause");
 const circle = document.querySelector("circle");
 
+//animate the circle
 const perimeter = circle.getAttribute("r") * 2 * Math.PI;
 circle.setAttribute("stroke-dasharray", perimeter);
+
+let currentOffset = 0;
+
+//listen to the input time from user
+let inputTime = 0;
+durationInput.addEventListener("input", (e) => {
+  inputTime = e.target.value;
+  console.log(inputTime);
+});
 
 const timer = new Timer(durationInput, startButton, pauseButton, {
   //the callback object tells the outside world that the Start/Tick/Complete events occurs
@@ -14,10 +25,12 @@ const timer = new Timer(durationInput, startButton, pauseButton, {
   },
 
   onTick() {
-    console.log("ticking down");
+    circle.setAttribute("stroke-dashoffset", currentOffset);
+    currentOffset = currentOffset - perimeter / (inputTime * 100);
   },
 
   onComplete() {
     console.log("timer completed");
+    currentOffset = 0;
   },
 });
